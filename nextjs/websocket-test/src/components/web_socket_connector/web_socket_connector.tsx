3b282b763d9481b6f47e9c0bd7ad023f2e0222d5
2017Yasu <yasu.nkym.96@gmail.com>
2024-04-13 19:05:38 +0900

    move socket io creation into effect

--- nextjs/websocket-test/src/components/web_socket_connector/web_socket_connector.tsx
+++ nextjs/websocket-test/src/components/web_socket_connector/web_socket_connector.tsx
@@ -1,25 +1,24 @@
-'use client'
+'use client';
 
 import { useEffectOnce } from '@/hooks/effect';
 import { useEffect } from 'react';
-import { io } from 'socket.io-client'
-
-const socket = io({ autoConnect: false });
+import { io } from 'socket.io-client';
 
 export default function WebSocketConnectorComponent() {
   useEffectOnce(async () => {
     await fetch('/api/ws', { method: 'POST' });
+    const socket = io({ autoConnect: false });
     if (!socket.connected) {
-    socket.connect();
-    socket.on('connect', () => {
-      console.log('connected!', socket.id);
-    });
-    socket.on('msg', (msg) => {
-      console.log(msg);
-    });
-    socket.on('disconnect', () => {
-      console.log('socket is disconnected', socket.id);
-    });
+      socket.connect();
+      socket.on('connect', () => {
+        console.log('connected!', socket.id);
+      });
+      socket.on('msg', (msg) => {
+        console.log(msg);
+      });
+      socket.on('disconnect', () => {
+        console.log('socket is disconnected', socket.id);
+      });
     }
     console.log('socket connected');
     return () => {
@@ -28,13 +27,11 @@ export default function WebSocketConnectorComponent() {
       socket.off('msg');
       socket.off('disconnect');
     };
-  })
+  });
 
   useEffectOnce(async () => {
-    console.log('another effect once')
-  })
+    console.log('another effect once');
+  });
 
-  return (
-    <h1>socket.io シンプルな接続例</h1>
-  )
+  return <h1>socket.io シンプルな接続例</h1>;
 }
