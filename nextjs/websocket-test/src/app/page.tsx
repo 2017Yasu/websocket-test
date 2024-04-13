f3a687d698150457209ffaeccb95e2b28ca8b90d
2017Yasu <yasu.nkym.96@gmail.com>
2024-04-13 10:41:29 +0900

    implement websocket connection

--- nextjs/websocket-test/src/app/page.tsx
+++ nextjs/websocket-test/src/app/page.tsx
@@ -2,11 +2,19 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import * as hello from '@/app/api/hello/route';
 import HelloMessage from '@/components/hello_message';
+import Link from 'next/link';
 
 export default async function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello Next.js</h1>
+      <div>
+        <ul>
+          <li>
+            <Link href={'/ws_test'} replace>WebSocket test page</Link>
+          </li>
+        </ul>
+      </div>
       <HelloMessage/>
     </main>
   );
